9d0eb9933fde65cbfed69e066a1c8586a6943a6c
Samantha <sambur_usa2k@yahoo.com>
2021-04-30 19:57:27 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_04_25_171308) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title", limit: 30
     t.string "body", limit: 1000
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
